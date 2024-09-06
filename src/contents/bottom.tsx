79696f3357a393ad95edb0a24083c03b84da840d
huangzhiqiang02 <huangzhiqiang02@zuoyebang.com>
2024-09-06 16:51:42 +0800

    feat: add content script ui

---
+++ src/contents/bottom.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+function bottom() {
+  return (
+    <div
+      style={{
+        position: "fixed",
+        right: 0,
+        bottom: 10,
+        backgroundColor: "blue"
+      }}>
+      bottom
+    </div>
+  )
+}
+
+export default bottom
---
+++ src/contents/sidebar.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+function ContentIndex() {
+  return (
+    <div
+      style={{
+        position: "fixed",
+        right: 0,
+        top: "50%",
+        backgroundColor: "red"
+      }}>
+      ContentIndex
+    </div>
+  )
+}
+
+export default ContentIndex
