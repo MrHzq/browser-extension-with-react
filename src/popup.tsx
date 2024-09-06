16481d33da540e83d1aeeb293cb588959171dbb0
huangzhiqiang02 <huangzhiqiang02@zuoyebang.com>
2024-09-06 14:17:15 +0800

    feat: add src dir

--- src/popup.tsx
+++ src/popup.tsx
@@ -6,15 +6,12 @@ function IndexPopup() {
   return (
     <div
       style={{
+        minWidth: 300,
+        display: "flex",
+        flexDirection: "column",
         padding: 16
       }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
+      <h2>Welcome to React Extension!</h2>
       <input onChange={(e) => setData(e.target.value)} value={data} />
       <a href="https://docs.plasmo.com" target="_blank">
         View Docs
